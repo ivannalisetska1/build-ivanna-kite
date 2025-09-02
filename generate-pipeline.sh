de721ecafa75405c0d275b8208706ef6ea69283a
ivannalisetska <155262606+ivannalisetska@users.noreply.github.com>
2025-09-02 10:22:08 -0600

    Create generate-pipeline.sh

---
+++ generate-pipeline.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+cat <<'YAML' | buildkite-agent pipeline upload
+steps:
+  - label: "Deploy A (sleep 60s)"
+    command: 'echo "A start"; date; sleep 60; echo "A done"; date'
+
+  - label: "Deploy B (sleep 30s)"
+    command: 'echo "B start"; date; sleep 30; echo "B done"; date'
+YAML
